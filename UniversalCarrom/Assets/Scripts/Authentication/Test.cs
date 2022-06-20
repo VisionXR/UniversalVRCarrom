a398cffa792b022a94f80858c88cfb261c0f05b8
manikarna <97171564+manikarna19@users.noreply.github.com>
2022-06-20 13:25:46 +0530

    Update Test.cs

--- UniversalCarrom/Assets/Scripts/Authentication/Test.cs
+++ UniversalCarrom/Assets/Scripts/Authentication/Test.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    public PlayfabManager PM;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      // if(Input.mo)
+        if (Input.GetMouseButton(0))
+          {
+
+        }
     }
 }
